a07aedece46d8636ee473bb9d9bd2a492570e887
vrshgnick <n-vereshaginn@mail.ru>
2023-01-07 15:25:02 +0300

    ogo, menya ebyt

--- 31.12.22/31.12.22 15 егэ (отрезки).py
+++ 31.12.22/31.12.22 15 егэ (отрезки).py
@@ -4,10 +4,10 @@
 
                  ## ДЛЯ ПОИСКА НАИМЕНЬШЕГО ##
 
-    # 1
-
-    # шаблонная часть
-
+#     1
+#
+#     шаблонная часть
+#
 # p1,p2,q1,q2 = 4, 15, 12, 20
 #
 # P = [i/10 for i in range(p1*10,p2*10 + 1)]
@@ -26,26 +26,26 @@
 # print(sorted(A))
 #
 # #[12,15] == 15 - 12 = 3
-
-    # 2
-
+#
+#     2
+#
 # p1,p2,q1,q2,r1,r2 = 10, 40, 5, 15, 35, 50
 #
 # P = [i/10 for i in range(p1*10,p2*10 + 1)]
 # Q = [i/10 for i in range(q1*10,q2*10 + 1)]
 # R = [i/10 for i in range(r1*10,r2*10 + 1)]
-#
-# A = set()
-#
-# def sol(x,A):
-#     return ((x in A) or (x in P)) or ((x in Q) <= (x in R))
-#
-# for x in [i/10 for i in range((5-1)*10, (50-1)*10 +1)]:
-#     if not sol (x,A):
-#         A.add(x)
-# print(sorted(A))
-#
-# # [5,10] 10-5=5(answer)
+
+A = set()
+
+def sol(x,A):
+    return ((x in A) or (x in P)) or ((x in Q) <= (x in R))
+
+for x in [i/10 for i in range((5-1)*10, (50-1)*10 +1)]:
+    if not sol (x,A):
+        A.add(x)
+print(sorted(A))
+
+# [5,10] 10-5=5(answer)
 
     # 3
 
